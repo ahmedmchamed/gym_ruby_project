0a7833715d0a741f3944b57012843ae23014a6f5
Ahmed Hamed <tunashark@gmail.com>
2020-06-04 23:11:36 +0100

    Added basic booking class

--- models/booking.rb
+++ models/booking.rb
@@ -1,7 +1,10 @@
 class Booking
 
     def initialize(options)
-
+        @id = options['id'].to_i() if options['id']
+        @member_id = options['member_id'].to_i()
+        @gymclass_id = options['gymclass_id'].to_i()
+        @membership_id = options['membership_id'].to_i()
     end
 
 end
