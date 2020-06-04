b58e351123d76e64ec45ccf19220ceff27c5a41b
Ahmed Hamed <tunashark@gmail.com>
2020-06-04 22:46:38 +0100

    added staff/memberships/classes/members tables

---
+++ db/gym_schema.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS classes;
+DROP TABLE IF EXISTS memberships;
+DROP TABLE IF EXISTS staff;
+
+CREATE TABLE staff (
+    id PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255);
+    privilege_level INT
+);
+
+CREATE TABLE memberships (
+    id PRIMARY KEY,
+    type VARCHAR(255),
+    status BOOLEAN
+);
+
+CREATE TABLE classes (
+    id PRIMARY KEY,
+    name VARCHAR(255),
+    duration INT,
+    intensity VARCHAR(255),
+    workout VARCHAR(255),
+    price INT
+);
+
+CREATE TABLE members (
+    id PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    age INT
+);
