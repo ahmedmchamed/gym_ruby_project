8a9566c2df462cf84bd3775e0a09085691c51a72
Ahmed Hamed <tunashark@gmail.com>
2020-06-04 23:16:25 +0100

    added basic gymclass

--- models/gymclass.rb
+++ models/gymclass.rb
@@ -1,1 +1,13 @@
-class  
+class Gymclass
+   
+    def initialize(options)
+        @id = options['id'].to_i() if options['id']
+        @start_time = options['start_time']
+        @duration = options['duration'].to_i() #in minutes
+        @intensity = options['intensity']
+        @workout = options['workout']
+        @capacity = options['capacity'].to_i()
+        @price = options['price'].to_i()
+    end
+    
+end
