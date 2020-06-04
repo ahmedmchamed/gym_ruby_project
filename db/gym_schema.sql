a5efbc8ff7bc56ee163aff8e190b891343224bb8
Ahmed Hamed <tunashark@gmail.com>
2020-06-04 22:53:47 +0100

    sql table works

--- db/gym_schema.sql
+++ db/gym_schema.sql
@@ -5,20 +5,20 @@ DROP TABLE IF EXISTS memberships;
 DROP TABLE IF EXISTS staff;
 
 CREATE TABLE staff (
-    id PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(255),
-    last_name VARCHAR(255);
+    last_name VARCHAR(255),
     privilege_level INT
 );
 
 CREATE TABLE memberships (
-    id PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     type VARCHAR(255),
     status BOOLEAN
 );
 
 CREATE TABLE classes (
-    id PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     duration INT,
     intensity VARCHAR(255),
@@ -27,14 +27,14 @@ CREATE TABLE classes (
 );
 
 CREATE TABLE members (
-    id PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     age INT
 );
 
 CREATE TABLE bookings (
-    id PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     member_id INT REFERENCES members(id),
     class_id INT REFERENCES classes(id),
     membership_id INT REFERENCES memberships(id)
