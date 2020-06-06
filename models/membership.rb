9c6e6a9bbab9b11c0619d29d73276518647111b3
Ahmed Hamed <tunashark@gmail.com>
2020-06-06 13:52:29 +0100

    added save method to Membership

--- models/membership.rb
+++ models/membership.rb
@@ -11,4 +11,12 @@ class Membership
         @status = options['status']
     end
 
+    def save()
+        sql = "INSERT INTO memberships
+        (type, status)
+        VALUES ($1, $2) RETURNING id;"
+        values = [@type, @status]
+        @id = SqlRunner.run(sql, values)[0]['id'].to_i()
+    end
+
 end
