fc934898cc771cd536800b42cffd9d0a710860ac
Ahmed Hamed <tunashark@gmail.com>
2020-06-06 15:31:06 +0100

    added find_all_memberships to Membership

--- models/membership.rb
+++ models/membership.rb
@@ -19,6 +19,14 @@ class Membership
         @id = SqlRunner.run(sql, values)[0]['id'].to_i()
     end
 
-    
+    def self.find_all_memberships()
+        sql = "SELECT * FROM memberships;"
+        memberships_hash_result = SqlRunner.run(sql)
+        return memberships_array_result = self.map_membership_data(memberships_hash_result)
+    end
+
+    def self.map_membership_data(membership_hash_data)
+        return membership_hash_data.map { |membership| Membership.new(membership) }
+    end
 
 end
